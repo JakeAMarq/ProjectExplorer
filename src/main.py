642f36b7ed4ad71d78e528c931902d4f825b8cbe
JakeAMarq <jacobmarquardt1@gmail.com>
2020-12-22 18:21:06 -0800

    got voice recognition and opening explorer at specific folder working

--- src/main.py
+++ src/main.py
@@ -1,29 +1,41 @@
-import pyaudio
-import wave
-import sys
+import speech_recognition as sr
+import os
+import re
 
-CHUNK = 1024
+def parsePath(string):
+    wordList = re.sub("[^\w]", " ",  string).split()
+    wordList = wordList[2:]
+    for word in wordList:
+        if (word == "slash"):
+            word = "/"
+    return wordList.join()
 
-if len(sys.argv) < 2:
-    print("Plays a wave file.\n\nUsage: %s resources/music.wav" % sys.argv[0])
-    sys.exit(-1)
+def openPath(path):
+    path = "C:/" + path
+    path = os.path.realpath(path)
+    os.startfile(path)  
 
-wf = wave.open(sys.argv[1], 'rb')
+def main():
+    while (True):
+        # obtain audio from the microphone
+        recognizer = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Say something!")
+            audio = recognizer.listen(source)
 
-p = pyaudio.PyAudio()
+        # recognize speech using Google Speech Recognition
+        try:
+            # for testing purposes, we're just using the default API key
+            # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+            # instead of `r.recognize_google(audio)`
+            voiceInput = recognizer.recognize_google(audio)
+            print("Google Speech Recognition thinks you said " + voiceInput)
+            if (voiceInput.startsWith("go to")):
+                path = parsePath(voiceInput)
+                openPath(voiceInput)
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                channels=wf.getnchannels(),
-                rate=wf.getframerate(),
-                output=True)
-
-data = wf.readframes(CHUNK)
-
-while data != '':
-    stream.write(data)
-    data = wf.readframes(CHUNK)
-
-stream.stop_stream()
-stream.close()
-
-p.terminate()
+main()
