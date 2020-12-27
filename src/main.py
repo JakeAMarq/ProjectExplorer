76a790ef2cb22aefcd4336a71ed91f48d73ea94f
JakeAMarq <jacobmarquardt1@gmail.com>
2020-12-27 12:43:08 -0800

    forgot to stage changes on last commit

--- src/main.py
+++ src/main.py
@@ -2,18 +2,81 @@ import speech_recognition as sr
 import os
 import re
 
-def parsePath(string):
-    wordList = re.sub("[^\w]", " ",  string).split()
-    wordList = wordList[2:]
-    for word in wordList:
-        if (word == "slash"):
-            word = "/"
-    return wordList.join()
+SEPARATOR_WORD = "slash"
 
-def openPath(path):
+def parse_path(string):
+    if not isinstance(string, str):
+        raise ValueError("string argument must be a str")
+
+    path = string.lower().replace(SEPARATOR_WORD, "/")
+    path = path.replace(" /", "/")
+    path = path.replace("/ ", "/")
+    return path
+
+def path_exists(path):
+    return os.path.exists(path)
+
+def open_path(path):
     path = "C:/" + path
     path = os.path.realpath(path)
     os.startfile(path)  
+
+def convert_to_snake_case(string):
+    return "_".join(string.split(" "))
+
+def remove_spaces(string):
+    return "".join(string.split(" "))
+
+def get_possible_directory_variations(directory):
+    return [directory, remove_spaces(directory), convert_to_snake_case(directory)]
+
+def convert_list_to_path(list):
+    return "/".join(list)
+
+def convert_path_to_list(path):
+    return path.split("/")
+
+def get_valid_path_variations(path):
+    """
+    Given a path, returns a list of paths found in the file system that either
+    exactly match the given path or match the path after applying common directory naming conventions
+
+    Keyword arguments:
+    path -- the path (string)
+    """
+
+    if not isinstance(path, str):
+        raise ValueError("path argument must be a str")
+
+    if path == "":
+        raise ValueError("path argument cannot be an empty str")
+
+    directories = convert_path_to_list(path)
+    drive = directories.pop(0)
+    validPathVariations = [[drive]]
+
+    if not path_exists(drive):
+        return []
+
+    for directory in directories:
+        if len(validPathVariations) == 0:
+            return []
+
+        possibleVariations = get_possible_directory_variations(directory)
+
+        newValidPathVariations = []
+        for path in validPathVariations:
+            pathString = convert_list_to_path(path)
+
+            for possibleVariation in possibleVariations:
+                possiblePath = pathString + "/" + possibleVariation
+                if path_exists(possiblePath):
+                    newValidPathVariations.append(path + [possibleVariation])
+
+        validPathVariations = newValidPathVariations[::]
+
+
+    return [convert_list_to_path(path) for path in validPathVariations]
 
 def main():
     while (True):
@@ -31,11 +94,19 @@ def main():
             voiceInput = recognizer.recognize_google(audio)
             print("Google Speech Recognition thinks you said " + voiceInput)
             if (voiceInput.startsWith("go to")):
-                path = parsePath(voiceInput)
-                openPath(voiceInput)
+                path = parse_path(voiceInput[6:])
+                open_path(path)
         except sr.UnknownValueError:
             print("Google Speech Recognition could not understand audio")
         except sr.RequestError as e:
             print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 main()
+# testcases = ["c: slash program files slash riot games", None, "", "c: idk", "c: slash idk"]
+
+# for case in testcases:
+#     print("Input: " + str(case))
+#     try:
+#         print("Output: " + str(get_valid_path_variations(parse_path(case))))
+#     except ValueError as error:
+#         print("Error raised: " + str(error))
